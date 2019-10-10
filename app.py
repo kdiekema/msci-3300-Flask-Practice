60952593e18e13471324d5b6e6d560a4db75be34
kdiekema <karina-diekema@uiowa.edu>
2019-10-09 23:01:00 -0500

    modified application to use Jinja template

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello world"
+    return render_template('index.html', pageTitle ='My Calculator')
 
 @app.route('/Karina')
 def Karina():
-    return "Hello Karina"
+    return render_template('karina.html', pageTitle = 'Karina')
 
 if __name__=='__main__':
     app.run(debug=True)
